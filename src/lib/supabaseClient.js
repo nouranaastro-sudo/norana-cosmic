4466b512cce578a561f222355bf4fcf937a3a8b2
nouranaastro-sudo <nouranaastro@gmail.com>
2026-09-22 03:04:18 +0300

    Add NORANA v6 src/lib/supabaseClient.js

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,4 @@
+import {createClient} from '@supabase/supabase-js';
+const url='https://edkgxgsoysbcqvidvglv.supabase.co';
+const key='sb_publishable_1W8uBPDCyPtsYuzPAK29dA__nQvPCuM';
+export const supabase=createClient(url,key);
